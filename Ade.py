bc43011287e2ccfded614e3db3ea404678e27a4f
moin21-meet <moin21@meet.mit.edu>
2019-07-22 10:02:20 -0400

    Stopping here

---
+++ Ade.py
@@ -0,0 +1,87 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+Traceback (most recent call last):
+  File "/home/student/moin21_lab2/meet2019y1lab2/lab_2.py", line 5, in <module>
+    import tutle # imports the turtle library
+ModuleNotFoundError: No module named 'tutle'
+>>> 
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+Traceback (most recent call last):
+  File "/home/student/moin21_lab2/meet2019y1lab2/lab_2.py", line 36, in <module>
+    turtle.manloop() # all turtle commands must go before this line!!!
+AttributeError: module 'turtle' has no attribute 'manloop'
+>>> 
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+>>> 
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+>>> 
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+========= RESTART: /home/student/moin21_lab2/meet2019y1lab2/lab_2.py =========
+
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+
+>>> 
+======== RESTART: /home/student/moin21_lab2/meet2019y1lab2/circle.py ========
+>>> 
